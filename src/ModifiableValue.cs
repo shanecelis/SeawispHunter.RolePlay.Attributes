af8a7a69dba546cdc0a4cf40ded5a7b0367b529c
Shane Celis <shane.celis@gmail.com>
2022-07-17 02:57:20 -0400

    legal: Add copyright header to new files.

--- src/ModifiableValue.cs
+++ src/ModifiableValue.cs
@@ -20,30 +20,6 @@ using System.Numerics;
 namespace SeawispHunter.RolePlay.Attributes {
   // XXX: Can IValue<T> work like a single value IEnumerable?
 
-/* IValue<T> notifies listeners when changed. That's it. */
-
-// XXX: Consider renaming IValue to IReadOnlyValue and IMutableValue to IValue.
-/* IMutableValue<T> is an IValue<T> that can be directly changed. */
-public interface IMutableValue<T> : IValue<T> {
-  /* We want this to be settable. */
-  new T value { get; set; }
-}
-
-/** This IModifiableValue<T> class is meant to capture values in games like health,
-    strength, etc. that can be modified by various, sometimes distal, effects. */
-public interface IModifiableValue<T> : IValue<T>, INotifyPropertyChanged {
-  T baseValue { get; set; }
-  // T value { get; }
-  /** The list implementation will handle chaining property change events. */
-  IPriorityCollection<IModifier<T>> modifiers { get; }
-  // event PropertyChangedEventHandler PropertyChanged;
-}
-
-/** We want to be able to specify a priority. */
-public interface IPriorityCollection<T> : ICollection<T> {
-  void Add(int priority, T modifier);
-}
-
 public static class ModifiableValue {
   public static IModifiableValue<T> FromFunc<T>(Func<T> f, out Action callOnChange) => new DerivedModifiableValue<T>(f, out callOnChange);
   public static IModifiableValue<T> FromFunc<T>(Func<T> f) => new DerivedModifiableValue<T>(f, out var callOnChange);
