013dca74e4c35e3bda1076ec7da853392be3eafd
Shane Celis <shane.celis@gmail.com>
2022-08-02 03:57:54 -0400

    feature: Allow modifier key with hover.

--- Samples~/Generic RPG/Scripts/TooltipOnHover.cs
+++ Samples~/Generic RPG/Scripts/TooltipOnHover.cs
@@ -23,22 +23,73 @@ public class TooltipOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitH
   [SerializeField] Tooltip tooltip;
   [FormerlySerializedAs("message")]
   [SerializeField] string _message;
+  [SerializeField] bool showBlankMessage = true;
+
+  [Serializable]
+  public class Criteria {
+    public bool all;
+    public KeyCode[] keys;
+
+    public bool IsMet() { 
+      if (all) {
+        foreach (var key in keys)
+          if (! Input.GetKey(key))
+            return false;
+        return true;
+      } else {
+        // any
+        bool hasOne = false;
+        foreach (var key in keys)
+          hasOne |= Input.GetKey(key);
+        return hasOne;
+      }
+    }
+  }
+
+  protected virtual bool showMessage
+    => (showBlankMessage || ! string.IsNullOrEmpty(message))
+       && AreValidKeys();
+
+  [SerializeField] private Criteria[] mustHave;
+  [SerializeField] private Criteria[] mustNotHave;
 
   public string message => messageDelegate == null ? _message : messageDelegate();
 
   public Func<string> messageDelegate;
   private bool entered = false;
 
+  void Awake() {
+    if (tooltip == null) {
+      Debug.LogWarning("No tooltip selected; disabling.", this.gameObject);
+      this.enabled = false;
+    }
+  }
+
   void Start() { }
 
+  bool AreValidKeys(){
+    bool valid = true;
+    foreach (var entry in mustHave)
+      valid &= entry.IsMet();
+    foreach (var entry in mustNotHave)
+      valid &= ! entry.IsMet();
+    return valid;
+  }
+
   public void OnPointerEnter(PointerEventData eventData) {
-    if (! string.IsNullOrEmpty(message)) {
+    if (showMessage) {
       tooltip.Show(message);
       entered = true;
     }
   }
 
   public void OnPointerExit(PointerEventData eventData) {
+    // If we have a child object drawn on top of us, this method will be called
+    // when the child is entered. But we only want to deal with someone exiting
+    // us, so we use `eventData.fullyExited` to discriminate.
+    if (! eventData.fullyExited)
+      return;
+
     tooltip.Hide();
     entered = false;
   }
