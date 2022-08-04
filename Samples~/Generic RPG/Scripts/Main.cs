e7b897a93c8dfe243f00b5fd5481842c14fb940f
Shane Celis <shane.celis@gmail.com>
2022-08-03 23:56:38 -0400

    feature: Add highlights in main code.

--- Samples~/Generic RPG/Scripts/Main.cs
+++ Samples~/Generic RPG/Scripts/Main.cs
@@ -8,6 +8,39 @@ namespace SeawispHunter.RolePlay.Attributes.Samples {
 
 public class Main : MonoBehaviour {
   [SerializeField] Tooltip helpTooltip;
+
+  [System.Serializable]
+  public class Highlights {
+    public KeyCode[] keys;
+    public Tooltip[] highlights;
+
+    public bool IsKeyDown() {
+      foreach (var key in keys)
+        if (Input.GetKeyDown(key))
+          return true;
+      return false;
+    }
+
+    public bool IsKeyUp() {
+      foreach (var key in keys)
+        if (Input.GetKeyUp(key))
+          return true;
+      return false;
+    }
+
+    public bool visible {
+      set {
+        foreach (var highlight in highlights)
+          if (value)
+            highlight.Show();
+          else
+            highlight.Hide();
+      }
+    }
+  }
+  [SerializeField] Highlights codeHighlights;
+  [SerializeField] Highlights inspectorHighlights;
+
   void Update() {
     if (Input.GetKeyDown(KeyCode.H)) {
       if (helpTooltip.isVisible)
@@ -22,6 +55,15 @@ public class Main : MonoBehaviour {
       SceneManager.LoadScene(scene.name);
     }
 
+    UpdateHighlights(codeHighlights);
+    UpdateHighlights(inspectorHighlights);
+  }
+
+  private void UpdateHighlights(Highlights highlights) {
+    if (highlights.IsKeyDown())
+      highlights.visible = true;
+    if (highlights.IsKeyUp())
+      highlights.visible = false;
   }
 
 }
